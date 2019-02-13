7597cdd57133d3df8084e1a37ec0d9e172e99cb2
thomas heap <s1832582@sms.ed.ac.uk>
2019-02-13 22:06:28 +0000

    shell scrips

---
+++ cluster_experiment_scripts/rsgan.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+#SBATCH -N 1	  # nodes requested
+#SBATCH -n 1	  # tasks requested
+#SBATCH --partition=LongJobs
+#SBATCH --gres=gpu:4
+#SBATCH --mem=12000  # memory in Mb
+#SBATCH --time=3-08:00:00
+
+export CUDA_HOME=/opt/cuda-9.0.176.1/
+
+export CUDNN_HOME=/opt/cuDNN-7.0/
+
+export STUDENT_ID=s1832582
+
+export LD_LIBRARY_PATH=${CUDNN_HOME}/lib64:${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
+
+export LIBRARY_PATH=${CUDNN_HOME}/lib64:$LIBRARY_PATH
+
+export CPATH=${CUDNN_HOME}/include:$CPATH
+
+export PATH=${CUDA_HOME}/bin:${PATH}
+
+export PYTHON_PATH=$PATH
+
+mkdir -p /disk/scratch/${STUDENT_ID}
+
+
+export TMPDIR=/disk/scratch/${STUDENT_ID}/
+export TMP=/disk/scratch/${STUDENT_ID}
+
+mkdir -p ${TMP}/datasets/
+export DATASET_DIR=${TMP}/datasets
+if [ -d "${DATASET_DIR}" ]; then
+echo "exists";
+fi
+# Activate the relevant virtual environment:
+
+source /home/s1832582/miniconda3/bin/activate mlp
+cd ..
+python copy_images.py --data ~/mlpractical/cluster_experiment_scripts/train_info.csv --output ${DATASET_DIR}/ --image_loc ~/paintings/train --style Impressionism
+python gan_loss_iter.py --image_size 128 --n_iter 51000 --input_folder /disk/scratch/s1832582 --output_folder ~/paintings_rsgan --n_gpu 4 --loss_D 5 --gen_every 50000 --extra_folder ~/paintings_rsgan/extraRS
+
