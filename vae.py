fb53f393f686e56dc89269f4fddee3ae7e5f3c34
thomas <thomaseheap@gmail.com>
2019-03-06 09:16:52 +0000

    Update vae.py

--- vae.py
+++ vae.py
@@ -117,15 +117,14 @@ model = VAE(image_channels=image_channels).to(device)
 #model.load_state_dict(torch.load('results/vae.torch', map_location='cpu'))
 optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
-# Reconstruction + KL divergence losses summed over all elements and batch
 def loss_fn(recon_x, x, mu, logvar):
-    BCE = F.binary_cross_entropy(recon_x, x, size_average=False)
-    # BCE = F.mse_loss(recon_x, x, size_average=False)
+    BCE = F.binary_cross_entropy(recon_x, x.view(-1, args.image_size * args.image_size), reduction='sum')
 
     # see Appendix B from VAE paper:
     # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+    # https://arxiv.org/abs/1312.6114
     # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-    KLD = -0.5 * torch.mean(1 + logvar - mu.pow(2) - logvar.exp())
+    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
 
     return BCE + KLD, BCE, KLD
 
