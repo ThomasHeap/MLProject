3308ccfc09b7304a75ff97eea5414487304e493f
thomas <thomaseheap@gmail.com>
2019-03-13 13:40:06 +0000

    Update imgsim.py

--- imgsim.py
+++ imgsim.py
@@ -15,35 +15,50 @@ from matplotlib.pyplot import imshow
 
 parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
 parser.add_argument('--targetPath', type=str,
-                    help=('Path to the target generated image'))
+                    help=('Path to the generated images'))
 parser.add_argument('--dataPath', type=str,
                     help=('Path to the dataset images'))
+parser.add_argument('--savePath', type=str,
+                    help=('Path to the save images'))
 args = parser.parse_args()
 
-targetImg = imread(args.targetPath)
 
+
+targetPath = pathlib.Path(args.targetPath)
 dataPath = pathlib.Path(args.dataPath)
 
-files = list(dataPath.glob('*.jpg')) + list(dataPath.glob('*.png'))
+targetFiles = list(targetPath.glob('*.jpg')) + list(targetPath.glob('*.png'))
+dataFiles = list(dataPath.glob('*.jpg')) + list(dataPath.glob('*.png'))
 
 def calculateDistance(i1, i2):
     return np.sum((i1-i2)**2)
 
-distances = []
 
-for f in files:
-    dataImg = transform.resize(imageio.imread(str(f)), (128,128,3)).astype(np.float32)
-    distances.append([calculateDistance(targetImg, dataImg), f])
-    print((len(distances) / len(files)) * 100)
+bestImages = []
+worstImages = []
+for g in targetFiles:
+    targetImg = imageio.imread(str(g))
+    distances = []
+    for f in dataFiles:
+        dataImg = transform.resize(imageio.imread(str(f)), (128,128,3)).astype(np.float32)
+        distances.append([f, calculateDistances(targetImg, dataImg)])
+
+    distances.sort(key=lambda x: x[1])
+    bestImages.append([g, distances[:5])
+    worstImages.append([g, distances[-5:]])
 
 
-distances.sort(key=lambda x: x[0])
 
-print(distances[:5])
-imtarg = Image.open(args.targetPath)
-imtarg.show()
-imtarg.save('target.png')
+bestImages.sort(key=lambda x: sum(x[1][:][1]))
+worstImages.sort(key=lambda x: -sum(x[1][:][1]))
+
 for i in range(5):
-    im = Image.open(distances[i][1])
-    im.show()
-    im.save('best{0}.png'.format(i))
+    worstIm = worstImages[i][0]
+    bestIm = bestImages[i][0]
+    worstIm.save('worst_sample{0}.png'.format(i))
+    bestIm.save('best_sample{0}.png'.format(i))
+    for j in range(5):
+        im1 = Image.open(bestImages[1][i][0])
+        im2 = Image.open(worstImages[1][i][0])
+        im1.save('{0}/best_nn{1}{2}.png'.format(args.savePath ,i,j) )
+        im2.save('{0}/worst_nn{1}{2}.png'.format(args.savePath ,i,j) )
